1cc184b89f0c413a57d77962d525d10c11628822
IchLanevich <107760870+IchLanevich@users.noreply.github.com>
2025-08-20 03:55:54 +0800

    Update index.js

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -3,5 +3,6 @@ import pokedexImg from './pokedex-img.png';
 import todoitImg from './todoit-img.png';
 import heroImg from './hero-img.jpg';
 import aboutmeImg from './aboutme-img.jpg';
+import hoobankImg from './hoobank-img.jpg'
 
-export { chatflowImg, pokedexImg, todoitImg, heroImg, aboutmeImg };
+export { chatflowImg, pokedexImg, todoitImg, heroImg, aboutmeImg, hoobankImg };
